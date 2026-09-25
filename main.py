7bc3568da51013e90b420559633f2db05dc00a49
Ruhama <ruhama.ugr-7672-18@aau.edu.et>
2026-09-25 17:55:22 +0300

    updated using flask

--- main.py
+++ main.py
@@ -4,10 +4,22 @@ from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, Con
 
 from handlers.register import start, join, ask_id, verify_id, ASKING_NAME, ASKING_ID
 
+from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running"
+
+def run_flask():
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
 load_dotenv()
 
 def main():
-    app = ApplicationBuilder().token(os.getenv("BOT_TOKEN")).build()
+    telegram_app = ApplicationBuilder().token(os.getenv("BOT_TOKEN")).build()
 
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("join", join)],
@@ -18,10 +30,11 @@ def main():
         fallbacks=[],
     )
 
-    app.add_handler(CommandHandler("start", start))
-    app.add_handler(conv_handler)
+    telegram_app.add_handler(CommandHandler("start", start))
+    telegram_app.add_handler(conv_handler)
 
-    app.run_polling()
+    telegram_app.run_polling()
 
 if __name__ == "__main__":
+    Thread(target=run_flask).start()
     main()
